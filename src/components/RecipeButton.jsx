b5c33a4c8abe241a064d077fe0aa5231ef024710
Apeksha Shrestha <apekshrestha22@gmail.com>
2025-04-25 07:24:22 +0000

    Revert "responsiveness fixed"
    This reverts commit f70a45265e059ea0c11877d3492d4f0c7aa592c7.

--- src/components/RecipeButton.jsx
+++ src/components/RecipeButton.jsx
@@ -1,6 +1,6 @@
 export default function RecipeButton(props){
   return (
-    <section className="bg-[#F0EFEB] flex flex-col sm:flex-row sm:items-end sm:justify-between gap-4 px-9 py-6 rounded-lg">
+    <section className="bg-[#F0EFEB] flex items-end justify-between px-9 py-6 rounded-lg">
       <div className="space-y-2">
         <h3 className="text-lg font-semibold">Ready for a recipe?</h3>
         <p className="text-sm text-gray-600">Generate a recipe from your list of ingredients.</p>
